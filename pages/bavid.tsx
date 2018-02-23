7d51c9599a45fd995de14e5e1e94efdb9ac0783e
Edward Bramanti <edward@bramanti.org>
2018-02-23 15:09:26 -0800

    Fix that good good

--- pages/bavid.tsx
+++ pages/bavid.tsx
@@ -6,17 +6,17 @@ import cx from "classnames";
 import Button from "@videoamp-private/preamp-ui/dist/src/components/buttons/Button";
 
 import {
-  buttonClassName,
-  buttonStyles,
-  edward
+    buttonClassName,
+    buttonStyles,
+    edward
 } from "../styles";
 
 export default () => <div>
     <Link href="/">
-    <Button flat className={cx("bavid", buttonClassName)}>
-    <p className="edward">Baebid</p>
-    <style jsx>{edward}</style>
-    </Button>
+        <Button flat className={cx("bavid", buttonClassName)}>
+            <p className="edward">Back to Home</p>
+            <style jsx>{edward}</style>
+        </Button>
     </Link>
-  {buttonStyles}
+    {buttonStyles}
 </div>

697ddff7a574ac98fe502f8a9927fd5dbe169ac6
Edward Bramanti <edward@bramanti.org>
2018-02-25 00:51:04 -0800

    Add static export support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 const withTypescript = require("@zeit/next-typescript");
 const withCSS = require("@zeit/next-css");
+const { exportPathMap } = require('nextjs-export-path-map')
+const path = require("path");
 
-module.exports = withTypescript(withCSS());
+module.exports = withTypescript(withCSS({
+  exportPathMap: exportPathMap.bind(null, path.join(__dirname, 'pages')),
+}));

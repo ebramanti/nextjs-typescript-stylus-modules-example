83a2d84b75bb441edb75ff3da6d9c6f7ea25d07e
David Ung <david@ung.io>
2018-02-27 09:48:57 -0800

    Add tabs

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Head from 'components/head'
 import Nav from 'components/nav'
 import cx from "classnames";
+import { TabsContainer, Toolbar } from '@videoamp-private/preamp-ui';
 
 import {
     dashboardStyles,
@@ -18,9 +19,19 @@ const tabs = [
     },
 ];
 
+const activeTabIndex = 0;
+const onTabChange = () => {};
+
 export default () => [
     <Head title="Template Manager"></Head>,
-    <header></header>,
+    <header>
+        <TabsContainer
+            toolbar={<Toolbar title="Template Manager"/>}
+            tabs={tabs}
+            activeTabIndex={activeTabIndex}
+            onTabChange={this.onTabChange}
+        />
+    </header>,
     <div className="outer">
         <aside></aside>
         <section>
